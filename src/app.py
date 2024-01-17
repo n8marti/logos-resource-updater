e2a91f2fa7eece9d8ccc16cffda67ccd3adabfc4
Nate Marti <nate_marti@sil.org>
2024-01-17 12:13:33 +0100

    print url list to stdout to enable redirect to file for wget

--- src/app.py
+++ src/app.py
@@ -27,7 +27,7 @@ def get_updates_list(updates_db, catalog_db):
 
 def set_downloads_list(updates):
     show_updates_list(updates)
-    ans = input(f"\nEnter the update numbers you wish to download, e.g. \"all\" or \"1,2,3\" [all]: ")
+    ans = get_input(f"\nEnter the update numbers you wish to download, e.g. \"all\" or \"1,2,3\" [all]: ")
     if not ans or ans.lower() == 'all':
         downloads_n = [n for n in range(1, len(updates)+1)]
     else:
@@ -43,27 +43,37 @@ def get_total_size(updates):
     return sum([u[1] for u in updates])
 
 def show_updates_list(updates):
-    print('\nUpdates list:')
+    to_stderr('\nUpdates list:')
     for n, u in enumerate(updates, start=1):
-        print(f"{n:>4}. {u[0]} ({str(round(u[1] / 1_000_000, 1))} MB)")
-    print(f"Total size: {get_total_size(updates) // 1_000_000} MB")
+        to_stderr(f"{n:>4}. {u[0]} ({str(round(u[1] / 1_000_000, 1))} MB)")
+    to_stderr(f"Total size: {get_total_size(updates) // 1_000_000} MB")
+
+def to_stderr(msg):
+    print(msg, file=sys.stderr)
+
+def get_input(msg):
+    out = sys.stdout
+    sys.stdout = sys.stderr
+    ans = input(msg)
+    sys.stdout = out
+    return ans
 
 def main():
     start_dir = Path(sys.argv[1])
     if not start_dir.is_dir():
-        print(f'Error: Not a valid folder: {start_dir}')
+        to_stderr(f'Error: Not a valid folder: {start_dir}')
         exit(1)
     # Find Logos.exe first to find install dir "Logos".
     logos_exe = utils.get_logos_exe(start_dir)
     if logos_exe is None:
-        print(f"Error: Logos.exe not found anywhere in {start_dir}")
+        to_stderr(f"Error: Logos.exe not found anywhere in {start_dir}")
         exit(1)
     # Get install dir "Logos" and wineprefix from Logos.exe path.
     logos_dir = utils.get_logos_dir(logos_exe)
     wineprefix = utils.get_wineprefix(logos_exe)
     user_id = logosmgr.get_user_id(logos_dir)
     if user_id is None:
-        print(f"Error: Logos user has not logged in.")
+        to_stderr(f"Error: Logos user has not logged in.")
         exit(1)
     # Get DB files.
     catalog_db = logosmgr.get_catalog_db(logos_dir, user_id)
@@ -72,6 +82,9 @@ def main():
     updates = get_updates_list(updates_db, catalog_db)
     # Set downloads list from user input.
     downloads = set_downloads_list(updates)
+    for d in downloads:
+        print(d[2])
+
 
 
 if __name__ == '__main__':
